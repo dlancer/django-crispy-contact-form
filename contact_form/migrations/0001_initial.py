c01d2a88cc9621b0482035dead1de281e13f1cfe
dlancer <dmdpost@gmail.com>
2014-11-27 22:20:55 +0500

    native migrations added for Django 1.7+

---
+++ contact_form/migrations/0001_initial.py
@@ -0,0 +1,68 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+import django.utils.timezone
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('sites', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Department',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('name', models.CharField(max_length=80)),
+                ('email', models.EmailField(max_length=254)),
+                ('phone', models.CharField(max_length=20, blank=True)),
+                ('site', models.ForeignKey(blank=True, to='sites.Site', null=True)),
+            ],
+            options={
+                'verbose_name': 'Department',
+                'verbose_name_plural': 'Departments',
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='Message',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('sender_name', models.CharField(max_length=80, verbose_name='Sender name')),
+                ('sender_email', models.EmailField(max_length=254, verbose_name='Sender email')),
+                ('message', models.TextField(max_length=4096, verbose_name='Message')),
+                ('ip', models.IPAddressField(null=True, verbose_name='IP', blank=True)),
+                ('date_created', models.DateTimeField(default=django.utils.timezone.now, verbose_name='Created')),
+                ('site', models.ForeignKey(blank=True, to='sites.Site', null=True)),
+            ],
+            options={
+                'verbose_name': 'Message',
+                'verbose_name_plural': 'Messages',
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='Subject',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('title', models.CharField(max_length=80)),
+                ('description', models.TextField(blank=True)),
+                ('department', models.ForeignKey(to='contact_form.Department')),
+                ('site', models.ForeignKey(blank=True, to='sites.Site', null=True)),
+            ],
+            options={
+                'verbose_name': 'Subject',
+                'verbose_name_plural': 'Subjects',
+            },
+            bases=(models.Model,),
+        ),
+        migrations.AddField(
+            model_name='message',
+            name='subject',
+            field=models.ForeignKey(verbose_name='Subject', to='contact_form.Subject'),
+            preserve_default=True,
+        ),
+    ]
