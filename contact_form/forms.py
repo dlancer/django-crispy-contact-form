8a4c812b0c456fefbe9264746d91a8cfeb5a2c2c
dlancer <dmdpost@gmail.com>
2015-08-17 17:28:43 +0500

    revert wrong super() correction

--- contact_form/forms.py
+++ contact_form/forms.py
@@ -136,4 +136,4 @@ class ContactFormCaptcha(ContactForm):
         self.helper.form_method = 'POST'
         self.helper.form_style = 'inline'
 
-        super(ContactFormCaptcha, self).__init__(*args, **kwargs)
+        super(ContactForm, self).__init__(*args, **kwargs)
