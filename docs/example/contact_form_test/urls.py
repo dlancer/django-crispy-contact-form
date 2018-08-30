cee1bf6545314d8df9451cae407e4ec425575731
dlancer <dmdpost@gmail.com>
2018-08-30 16:03:02 +0500

    example app updated

--- docs/example/contact_form_test/urls.py
+++ docs/example/contact_form_test/urls.py
@@ -1,13 +1,14 @@
-from django.conf.urls import patterns, include, url
+
+from django.conf.urls import include, url
 
 from django.contrib import admin
 
 admin.autodiscover()
 
-urlpatterns = patterns(
+urlpatterns = [
     '',
     url(r'^captcha/', include('captcha.urls')),
     url(r'^contact_form/', include('contact_form.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
-)
+]
